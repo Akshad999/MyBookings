340cb8b5ca4759ba61988b9162bf3887bf0eae61
Akshad999 <patialakshad@gmail.com>
2025-11-10 17:40:37 +0530

    Update payment.js

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+//const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Event = require('../models/Event');
 const Ticket = require('../models/Ticket');
 const User = require('../models/User');
