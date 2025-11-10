b8bcb315b6a22e4a431218b1701d599b2b78e50c
Akshad999 <patialakshad@gmail.com>
2025-11-10 18:16:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -118,5 +118,10 @@ process.on('SIGTERM', () => {
     console.log('✅ Process terminated');
   });
 });
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 module.exports = app;
